423ac1d15261ce5c486695efdfc48aaa2961b75e
Vyacheslav Vinichenko <syavik2001@gmail.com>
2025-07-01 06:44:09 +0300

    Удалена неиспользуемая папка app_backup_multilang для успешной сборки

--- app_backup_multilang/[locale]/simple/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SimplePage() {
-	return (
-		<div className="p-8">
-			<h1 className="text-2xl font-bold">Простая страница работает!</h1>
-			<p>Если вы видите это сообщение, значит роутинг работает корректно.</p>
-		</div>
-	);
-}
--- app_backup_multilang/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function RootPage() {
-	// Middleware автоматически обработает редирект
-	// Но на всякий случай добавим fallback
-	redirect("/uk");
-}
